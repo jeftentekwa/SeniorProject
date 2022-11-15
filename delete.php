e7acac7e4441dca24b823a766efdd011a6e2c6dd
jeftentekwa <93457897+jeftentekwa@users.noreply.github.com>
2022-11-15 17:14:20 -0500

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,46 @@
+<?php
+   $CarID = $_POST["CarID"];
+   $VinNum = $_POST["VinNumber"];
+   $Price = $_POST["Price"];
+   $mileage = $_POST["mileage"];
+   $Make = $_POST["make"];
+   $stockNumber = $_POST["stockNumber"];
+   $model = $_POST["model"];
+   $year = $_POST["year"];
+   $trim= $_POST["trim"];
+   $bodyType = $_POST["bodyType"];
+   $vehicleType= $_POST["vehicleType"];
+   $driveTrain = $_POST["driveTrain"];
+   $transmission= $_POST["transmission"];
+   $fuelType = $_POST["fuelType"];
+  $engineSize= $_POST["engineSize"]; 
+   $engineBlock = $_POST["engineBlock"];
+   $pFrom = $_POST["pFrom"];
+   $street = $_POST["street"];
+   $city = $_POST["City"];
+   $state = $_POST["state"];
+   $zip = $_POST["zipCode"];
+   $Price = $_POST["Price"]; 
+
+   // Database connection 
+   $host = "localhost";
+   $dbname ="mysql";
+   $username = "root";
+   $password ="";
+
+  $conn= mysqli_connect($host, $username, $password, $dbname); 
+   if(mysqli_connect_errno())
+   {
+    die("connection error ".mysqli_connect_error());
+   }
+   $sql = "DELETE FROM usedcarinventory2022 WHERE CarID = '$CarID'";
+   if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+  } else {
+    echo "Error deleting record: " . $conn->error;
+  }
+
+   
+
+
+?>
